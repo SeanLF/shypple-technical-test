e985e1f1d25a39875ed362ac96daa28623dbc248
Sean Floyd <seanfloyd@icloud.com>
2020-06-17 23:28:23 -0400

    Add helper functions

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -22,6 +22,7 @@ export default {
   },
   data: () => ({
     grid: null,
+    fibonacciSequence: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233],
   }),
   props: {
     gridSize: Number,
@@ -42,9 +43,19 @@ export default {
       },
     },
   },
+  // Computed caches results, and lazy loads
   computed: {
+    // Flattens the grid
     flatGrid() {
       return this.grid.flat();
+    },
+    // Cells in the grid that have a count that is a Fibonacci number
+    fibonacciCells() {
+      return this.flatGrid.filter(this.cellContainsFibonacci);
+    },
+    // Counts the number of distinct Fibonacci numbers in the grid
+    fibonacciUniqueCount() {
+      return [...new Set(this.fibonacciCells.map((cell) => cell.count))].length;
     },
   },
   methods: {
@@ -58,6 +69,26 @@ export default {
       });
       // Return the unique cells as an array
       return [...results];
+    },
+    // Checks if a number is a valid Fibonacci number, and keeps it stored in data
+    isFibonacci(n) {
+      // skip nulls
+      if (n === null) return false;
+
+      let maxFibonacciNumber = Math.max(...this.fibonacciSequence);
+
+      // If the number is in the range of stored fibonacci, is it in the list?
+      if (n <= maxFibonacciNumber) return this.fibonacciSequence.indexOf(n) !== -1;
+
+      // The number is not in the range, compute the following fibonacci numbers until gte n
+      while (maxFibonacciNumber <= n) {
+        maxFibonacciNumber = this.fibonacciSequence.slice(-2).reduce((a, b) => a + b, 0);
+        this.fibonacciSequence.push(maxFibonacciNumber);
+      }
+      return this.isFibonacci(n);
+    },
+    cellContainsFibonacci(cell) {
+      return this.isFibonacci(cell.count);
     },
     clicked(rowIndex, columnIndex) {
       // Get the cells in the same row and column and increment them each by 1
